cb7331cccf77c59f46e5db30aca0bbbaaa19b54a
Flori-Soft-open <computerflo12+Github@gmail.com>
2024-07-14 14:23:37 +0200

    Add files via upload
    This is my own JS library you can import it for free. You can use this library in your Web-page for free

---
+++ Flori-JS-library.js
@@ -0,0 +1,49 @@
+FloriV =1.2
+function StartStopwatch() { // Use that to start the stopwatch
+    V = 1
+    StopwatchTime = 0
+    
+        if (V = 1) {
+            setTimeout(function() {
+                StopwatchTime = StopwatchTime + 1 // Ad one second to StopwatchTime 
+
+        
+                },1000); // Stopwatch different
+            }
+        
+        
+
+            
+        }
+
+
+
+
+function StopStopwatchst() { // Use this to stop the Stopwatch
+    V = 0
+
+}
+
+function buttonrun() { // your button "button" starts your function "brun"
+    document.getElementById("button").addEventListener("click", function() {
+        brun()
+    })
+}
+function FloriJSver() {
+    console.log ("Flori-Soft-open-source Software (Flori-JS) " + Flori-V)
+}
+
+function florisoftwarealert() {
+    alert ("This Software is use a Flori-Software library (Flori-JS) in Version " + FloriV );
+    alert (";-)")
+
+}
+
+function spamconsole() {
+    Num = 0
+    while (True) {
+        console.log (Num)
+        Num = Num + 1
+    }
+}
+
